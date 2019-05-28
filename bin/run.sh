b683227f9520332f209ba2a2e4651ee180c69873
gosho-kazuya <ketsume0211@gmail.com>
2019-05-29 02:56:50 +0900

    sorry wip

--- bin/run.sh
+++ bin/run.sh
@@ -29,15 +29,16 @@ main() {
     log 'Welcome to metabase playbook shell.'
     log 'We do not collect any sensitive information, like password.'
     echo
-    log 'step 1. Input IP address of your Metabase instance.'
-    log 'e.g.) 172.31.0.14'
-    read -p '> ' ip
+    log 'step 1. Input information address of your Metabase instance.'
+    echo
+    log 'IP address to install. e.g.) 172.31.0.14'
+    read -p 'ip > ' ip
 
-    log 'step 2. Input MySQL password to store Metabase data.'
-    read -p '> ' mysql_password
+    log 'Input MySQL password to store Metabase data. e.g.) q2oenqnbowh20bv93ge4'
+    read -p 'MySQL Password > ' mysql_password
 
-    echo "ip: $ip"
-    echo "mysql_password: $mysql_password"
+    log 'Input SSH Key path to connect the instance. default) $HOME/.ssh/id_rsa'
+    read -p 'ssh key path > ' ssh_key_path
 
     log 'Sorry, this script is WIP.'
 }
